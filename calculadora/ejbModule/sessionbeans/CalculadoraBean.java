93e6cb4f7710885934bbb6bd3729c4ebfee3670d
Anderson Luiz de Almeida Prado <andersonlprado@gmail.com.br>
2012-08-14 11:09:03 -0300

    Primeiro envio

---
+++ calculadora/ejbModule/sessionbeans/CalculadoraBean.java
@@ -0,0 +1,14 @@
+package sessionbeans;
+
+import javax.ejb.Local;
+import javax.ejb.Stateless;
+
+@Stateless
+@Local(Calculadora.class)
+public class CalculadoraBean implements Calculadora {
+
+	@Override
+	public double soma(double a, double b) {
+		return a + b;
+	}
+}
